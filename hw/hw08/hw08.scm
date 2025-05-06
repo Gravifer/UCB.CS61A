2bf6146ab058730f408fb6c82877ed9fd2088d25
Gravifer <hamburgmega@gmail.com>
2025-05-06 14:19:04 +0800

    Finished hw08

--- hw/hw08/hw08.scm
+++ hw/hw08/hw08.scm
@@ -1,7 +1,38 @@
-(define (ascending? s) 'YOUR-CODE-HERE)
+(define (ascending? s)
+  (if (null? s)
+      #t
+      (let ((first (car s))
+            (rest (cdr s)))
+        (if (null? rest)
+            #t
+            (and
+              (>= (car rest) first)
+              (ascending? rest))))))
 
-(define (my-filter pred s) 'YOUR-CODE-HERE)
+(define (my-filter pred s)
+  (if (null? s)
+      '()
+      (let ((first (car s))
+            (rest (cdr s)))
+        (if (pred first)
+            (cons first (my-filter pred rest))
+            (my-filter pred rest)))))
 
-(define (interleave lst1 lst2) 'YOUR-CODE-HERE)
+(define (interleave lst1 lst2)
+  (if (null? lst1)
+      lst2
+      (if (null? lst2)
+          lst1
+          (cons (car lst1)
+                (cons (car lst2) (interleave (cdr lst1) (cdr lst2)))))))
 
-(define (no-repeats s) 'YOUR-CODE-HERE)
+(define (no-repeats s)
+  (if (null? s)
+      '()
+      (let ((first (car s))
+            (rest (cdr s)))
+        (cons first
+              (no-repeats 
+                (my-filter
+                  (lambda (x) (not (equal? x first)))
+                  rest))))))
