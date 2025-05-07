ee28f38d0321bcd1f370061bedce0959daf0a179
Gravifer <hamburgmega@gmail.com>
2025-05-07 19:32:10 +0800

    Finished lab11

--- lab/lab11/lab11.scm
+++ lab/lab11/lab11.scm
@@ -1,15 +1,24 @@
 (define (if-program condition if-true if-false)
-  'YOUR-CODE-HERE)
+  `(if ,condition
+     (begin ,if-true)
+     (begin ,if-false)))
 
 (define (square n) (* n n))
 
-(define (pow-expr base exp) 'YOUR-CODE-HERE)
+(define (pow-expr base exp) ; fast pow constructed
+  (cond 
+    ((= exp 0) 1)
+    ((= exp 1) `(* ,base 1))
+    ((= (remainder exp 2) 0) `(square ,(pow-expr base (/ exp 2))))
+    (else `(* ,base ,(pow-expr base (- exp 1))))
+  ))
 
 (define-macro (repeat n expr)
-  `(repeated-call ,n ___))
+  `(repeated-call ,n (lambda () ,expr))) ; use a thunk to defer
+; the interface of repeated-call dictates this; otherwise one may also use a 
 
 ; Call zero-argument procedure f n times and return the final result.
 (define (repeated-call n f)
   (if (= n 1)
-      ___
-      (begin ___ ___)))
+      (f)
+      (begin (f) (repeated-call (- n 1) f))))
